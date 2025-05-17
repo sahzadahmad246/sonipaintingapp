97ac5b11a2974dd51d67d0c4225c03527b95f72c
Shahzad Ahmad <sahzadahmad246@gmail.com>
2025-05-17 16:51:58 +0530

    added search console tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,30 +1,34 @@
-import type React from "react"
-import type { Metadata } from "next"
-import { Inter } from "next/font/google"
-import "./globals.css"
+import type React from "react";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
 
-import ClientWrapper from "@/components/helpers/ClientWrapper"
+import ClientWrapper from "@/components/helpers/ClientWrapper";
 
-const inter = Inter({ subsets: ["latin"] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Soni Painting- Interior Contractor",
   description: "Interior painting, POP, carpentry, and tiling services",
-}
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
       <head>
         <link rel="icon" href="/logo.png" type="image/png" />
+        <meta
+          name="google-site-verification"
+          content="NXJfLPwHsoTaLMkWZbm8LJPG6qxr_W8OiMIxXoVYl-0"
+        />
       </head>
       <body className={inter.className}>
         <ClientWrapper>{children}</ClientWrapper>
       </body>
     </html>
-  )
+  );
 }
